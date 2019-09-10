4dedccb1c03751ad62f6a2d9ac809b95a754b8d5
spaut33 <roman.petrakov+git@gmail.com>
2019-09-10 12:22:12 +0300

    Initial commit

---
+++ trim_hatches.py
@@ -0,0 +1,41 @@
+import rhinoscriptsyntax as rs
+
+
+hatch = rs.GetObject(message="Pick any hatch", filter=rs.filter.hatch)
+trim_lines = rs.GetObject(message="Select cutting lines", filter=rs.filter.curve)
+
+selected_objects = []
+selected_objects.append(trim_lines)
+
+if hatch:
+    # Store original hatch settings
+    hatch_pattern = rs.HatchPattern(hatch)
+    hatch_rotation = rs.HatchRotation(hatch)
+    hatch_scale = rs.HatchScale(hatch)
+    
+    # Make hatch solid so we able to explode it and get surface instead
+    if hatch_pattern != "Solid":
+        rs.HatchPattern(hatch, "Solid")
+    
+    dup_border_surface = rs.ExplodeHatch(hatch)[0]
+    rs.SurfaceIsocurveDensity(dup_border_surface, 100)
+    selected_objects.append(dup_border_surface)
+    rs.SelectObjects(selected_objects)
+    rs.HideObject(hatch)
+    rs.Command("_Trim")
+    trimmed_surface = rs.LastCreatedObjects()
+    
+    new_borders = []
+    if trimmed_surface:
+        new_borders.append(rs.DuplicateSurfaceBorder(trimmed_surface))
+        selected_objects.append(new_borders)
+        # Keep trimming lines but everything else will be deleted further
+        selected_objects.remove(trim_lines)
+        new_hatches = rs.AddHatches(new_borders, hatch_pattern, hatch_scale, hatch_rotation)
+        rs.MatchObjectAttributes(new_hatches, hatch)
+        rs.ShowObject(new_hatches)
+        rs.DeleteObjects(selected_objects)
+    else:
+        print("No trimmed surfaces was created.")
+else:
+    print("No hatches was selected.")
